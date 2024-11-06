562f0243d143169342751962d6fc9438d2f52ded
IngvarOrlov <kampaiski@gmail.com>
2024-11-06 18:41:09 +0300

    Added some funcs with echo

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
 
-BOT_TOKEN = '6059463480:AAGGaRv1g_Cp1TolFz-023EQzX2sXvQauEQ'
+from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+@dp.message(F.sticker)
+async def send_sticker_echo(message: Message):
+    await message.reply_sticker(message.sticker.file_id)
 
 
 @dp.message(Command(commands=["start"]))
@@ -21,7 +27,13 @@ async def process_help_command(message: Message):
         'Напиши мне что-нибудь и в ответ '
         'я пришлю тебе твое сообщение'
     )
+@dp.message(F.photo)
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
 
+@dp.message(F.voice)
+async def send_voice_echo(message: Message):
+    await message.reply_voice(message.voice.file_id)
 
 @dp.message()
 async def send_echo(message: Message):
